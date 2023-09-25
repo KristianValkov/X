ce4a56ae11f3f25e1769ac63083dc98ac07e61a9
KristianValkov <120708602+KristianValkov@users.noreply.github.com>
2023-09-25 11:08:53 +0300

    Add files via upload

---
+++ Franklin/pregovor/pregovor/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace pregovor
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //kolko dushi ste v chas
+            
+            int x;
+            do
+            {
+                Console.WriteLine("Kolko choveka ste v chas?");
+                x = int.Parse(Console.ReadLine());
+                if (x < 1)
+                {
+                    Console.WriteLine("tvurde malko ste");
+                }
+                else
+                if (x > 14)
+                {
+                    Console.WriteLine("tvurde mnogo ste");
+                }
+                else
+                if (x>1&x<=14)
+                {
+                    Console.WriteLine("Koi e umen?");
+                }
+                else
+                {
+                    Console.WriteLine("Nevalidno chislo");
+                }
+            }
+            while (x<1 & x>=14);
+            
+
+
+
+
+        }
+    }
+}
