8b40d9690cca4f503ce9ab62b2657d6aa5738b1e
KristianValkov <120708602+KristianValkov@users.noreply.github.com>
2023-10-16 12:25:16 +0300

    Add files via upload

---
+++ Task2Numbers/N/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace N
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(9, 105);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(41, 13);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Изход-";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(130, 53);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(112, 65);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Click";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(12, 69);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 2;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(12, 53);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(80, 13);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "Въведи число:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 235);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label lbl2;
+    }
+}
+
---
+++ Task2Numbers/N/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace N
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            lbl1.Text = txt1.Text;
+            switch (txt1.Text)
+            {
+
+                case "0": MessageBox.Show("Goood"); txt1.BackColor = Color.Green;lbl1.Text = "Нула"; break;
+                case "1": MessageBox.Show("Goood"); txt1.BackColor = Color.Green; lbl1.Text = "Едно"; break;
+                case "2": MessageBox.Show("Goood");txt1.BackColor = Color.Green; lbl1.Text = "Две"; break;
+                case "3": MessageBox.Show("Goood"); txt1.BackColor = Color.Green; lbl1.Text = "Три"; break;
+                case "4": MessageBox.Show("Goood"); txt1.BackColor = Color.Green; lbl1.Text = "Четири"; break;
+                case "5": MessageBox.Show("Goood"); txt1.BackColor = Color.Green; lbl1.Text = "Пет"; break;
+                case "6": MessageBox.Show("Goood"); txt1.BackColor = Color.Green; lbl1.Text = "Шест"; break;
+                case "7": MessageBox.Show("Goood"); txt1.BackColor = Color.Green; lbl1.Text = "Седем"; break;
+                case "8": MessageBox.Show("Goood"); txt1.BackColor = Color.Green; lbl1.Text = "Осем"; break;
+                case "9": MessageBox.Show("Goood"); txt1.BackColor = Color.Green; lbl1.Text = "Девет"; break;
+                default:
+                    lbl1.Text = "Няма такова число";
+                    txt1.BackColor = Color.Red;
+                    MessageBox.Show("Fk-Въведи правилно число","Error",MessageBoxButtons.YesNoCancel,MessageBoxIcon.Error);
+                    
+                    break;
+            }
+            
+
+        }
+    }
+}
