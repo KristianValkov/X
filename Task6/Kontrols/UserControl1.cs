52c5d8a4bcc67161ba618505e087e958da43876c
KristianValkov <120708602+KristianValkov@users.noreply.github.com>
2023-11-20 12:13:46 +0200

    Add files via upload

---
+++ Task6/Kontrols/UserControl1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kontrols
+{
+    public partial class userControl1 : UserControl
+    {
+        public userControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
