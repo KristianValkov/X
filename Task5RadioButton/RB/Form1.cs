d7bb3cf754974ccc6d5ec516ba85d9756db45465
KristianValkov <120708602+KristianValkov@users.noreply.github.com>
2023-11-20 10:43:08 +0200

    Add files via upload

---
+++ Task5RadioButton/RB/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox1.Text);
+            int n2 = int.Parse(textBox2.Text);
+            int all = n2 + n;
+            if(radioButton1.Checked== true)
+            {
+                lblB.Text = textBox2.Text;
+            }
+            if (radioButton2.Checked == true)
+            {
+                lblB.Text = textBox1.Text;
+            }
+            if (radioButton3.Checked == true)
+            {
+                lblB.Text = all.ToString();
+                
+            }
+        }
+    }
+}
